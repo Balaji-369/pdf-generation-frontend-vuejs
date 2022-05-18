f4106527b7e6efe45a11b59dcfcf3bccf5a7e962
XxaynkaranxX <aynkaran@thinkbigsoft.com>
2022-05-18 12:53:12 +0530

    commit on master

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 createApp(App).mount('#app')
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
